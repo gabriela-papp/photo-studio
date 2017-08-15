1c3c52d3bcb54313787a693f54b2669b2301879f
gabriela pappova <gpgabika@gmail.com>
2017-08-15 23:42:54 +0100

    js added

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 $('.second_section h1').addClass('animated fadeInUp');
+
+$('.next').click(function(){
+	var current = $('.testimon-body.active');
+	var next = current.next();
+	current.removeClass('active');
+	next.addClass('active');
+});
