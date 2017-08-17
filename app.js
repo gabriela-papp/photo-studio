c05684455b44032fdb298d045747000163f51afe
gabriela pappova <gpgabika@gmail.com>
2017-08-17 21:34:02 +0100

    testimon work

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 $('.second_section h1').addClass('animated fadeInUp');
 
 $('.next').click(function(){
-	var current = $('.testimon-body.active');
+	var current = $('.testimon-body');
 	var next = current.next();
+
 	current.removeClass('active');
 	next.addClass('active');
 });
